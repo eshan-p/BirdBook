cf55733679f55a7cedeb84e8ba340092b27d7c33
Eshan Patel <133532859+eshan-p@users.noreply.github.com>
2026-01-25 15:32:31 -0600

    fixed header

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -24,7 +24,7 @@ function Header() {
     { label: 'Groups', path: '/groups' },
     { label: 'Friends', path: '/friends' },
     { label: 'Birds', path: '/birds' },
-    { label: 'Profile', path: '/profile' }
+    { label: 'Profile', path: '/profile' },
     { label: 'Users', path: '/users' }
   ];
 
